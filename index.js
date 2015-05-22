d56106d0ebf19c6d0f7f2992ef0aa103368bd117
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2015-05-22 17:05:33 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var path = require("path");
+var fs = require("fs");
+var _ = require("underscore");
+
+module.exports = function(schemaPath) {
+  var files = fs.readdirSync(path.resolve(schemaPath));
+
+  var schemas = {};
+
+  _.each(files, function(file) {
+    if (file !== "schemas.js") {
+      schemas[file.split(".")[0]] = require(path.resolve(path.join(schemaPath, file));
+    }
+  });
+
+  return schemas;
+}
